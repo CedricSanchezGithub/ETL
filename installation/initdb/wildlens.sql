b7f24a3ffaec0424226c4a8c134655d096da28b2
Jason-24 <135524962+Jason-Tch24@users.noreply.github.com>
2025-03-01 13:33:27 +0100

    update database schema (wildlens BD) as MCD

--- installation/initdb/wildlens.sql
+++ installation/initdb/wildlens.sql
@@ -1,155 +1,124 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `wildlens` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `grafana_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+GRANT ALL PRIVILEGES ON grafana_db.* TO 'grafana'@'%';
+FLUSH PRIVILEGES;
+
+USE `wildlens`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: mysql
--- Generation Time: Feb 07, 2025 at 09:30 AM
--- Server version: 8.0.33
--- PHP Version: 8.2.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: wildlens
+-- ------------------------------------------------------
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `wildlens`
+-- Table structure for table `wildlens_etat`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `wildlens_d1_images`
---
-CREATE DATABASE IF NOT EXISTS wildlens;
-CREATE DATABASE IF NOT EXISTS grafana_db;
-
--- Assurer que Grafana a accès à sa propre base
-GRANT ALL PRIVILEGES ON grafana_db.* TO 'grafana'@'%';
-FLUSH PRIVILEGES;
-
-
-
-CREATE TABLE `wildlens_d1_images` (
-  `id_image` bigint UNSIGNED NOT NULL,
-  `url_image` text NOT NULL
+DROP TABLE IF EXISTS `wildlens_etat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `wildlens_etat` (
+  `id_etat` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_etat`),
+  UNIQUE KEY `id_zone` (`id_etat`),
+  UNIQUE KEY `type_UNIQUE` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `wildlens_d2_zone`
---
-
-CREATE TABLE `wildlens_d2_zone` (
-  `id_zone` bigint UNSIGNED NOT NULL,
-  `nom_zone` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `wildlens_d3_especes`
+-- Dumping data for table `wildlens_etat`
 --
 
-CREATE TABLE `wildlens_d3_especes` (
-  `id_espece` bigint UNSIGNED NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `famille` varchar(255) NOT NULL,
-  `nom_latin` varchar(255) NOT NULL,
-  `description` text,
-  `population_estimee` int DEFAULT NULL,
-  `plus_grande_menace` text,
-  `localisation` text,
-  `slogan` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+LOCK TABLES `wildlens_etat` WRITE;
+/*!40000 ALTER TABLE `wildlens_etat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wildlens_etat` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `wildlens_facts`
 --
 
+DROP TABLE IF EXISTS `wildlens_facts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `wildlens_facts` (
-  `id_fact` bigint UNSIGNED NOT NULL,
-  `id_image` int DEFAULT NULL,
-  `id_zone` int DEFAULT NULL,
-  `id_espece` int DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `latitude` decimal(9,6) DEFAULT NULL,
-  `longitude` decimal(9,6) DEFAULT NULL
+  `id_espece` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `famille` varchar(255) NOT NULL,
+  `nom_latin` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `population_estimee` double NOT NULL,
+  `localisation` varchar(255) NOT NULL,
+  `nom_en` varchar(255) NOT NULL,
+  `nombre_image` int NOT NULL,
+  `coeff_multiplication` int NOT NULL,
+  PRIMARY KEY (`id_espece`),
+  UNIQUE KEY `id_fact` (`id_espece`),
+  KEY `idx_wildlens_facts_image` (`famille`),
+  KEY `idx_wildlens_facts_zone` (`nom_latin`),
+  KEY `idx_wildlens_facts_espece` (`description`),
+  KEY `idx_wildlens_facts_timestamp` (`population_estimee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `wildlens_facts`
 --
 
---
--- Indexes for table `wildlens_d1_images`
---
-ALTER TABLE `wildlens_d1_images`
-  ADD PRIMARY KEY (`id_image`),
-  ADD UNIQUE KEY `id_image` (`id_image`);
+LOCK TABLES `wildlens_facts` WRITE;
+/*!40000 ALTER TABLE `wildlens_facts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wildlens_facts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Indexes for table `wildlens_d2_zone`
---
-ALTER TABLE `wildlens_d2_zone`
-  ADD PRIMARY KEY (`id_zone`),
-  ADD UNIQUE KEY `id_zone` (`id_zone`);
-
---
--- Indexes for table `wildlens_d3_especes`
---
-ALTER TABLE `wildlens_d3_especes`
-  ADD PRIMARY KEY (`id_espece`),
-  ADD UNIQUE KEY `id_espece` (`id_espece`);
-
---
--- Indexes for table `wildlens_facts`
---
-ALTER TABLE `wildlens_facts`
-  ADD PRIMARY KEY (`id_fact`),
-  ADD UNIQUE KEY `id_fact` (`id_fact`),
-  ADD KEY `idx_wildlens_facts_image` (`id_image`),
-  ADD KEY `idx_wildlens_facts_zone` (`id_zone`),
-  ADD KEY `idx_wildlens_facts_espece` (`id_espece`),
-  ADD KEY `idx_wildlens_facts_timestamp` (`timestamp`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Table structure for table `wildlens_images`
 --
 
---
--- AUTO_INCREMENT for table `wildlens_d1_images`
---
-ALTER TABLE `wildlens_d1_images`
-  MODIFY `id_image` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+DROP TABLE IF EXISTS `wildlens_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `wildlens_images` (
+  `id_image` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `image` blob NOT NULL,
+  `id_espece` bigint unsigned NOT NULL,
+  `id_etat` bigint unsigned NOT NULL,
+  PRIMARY KEY (`id_image`),
+  UNIQUE KEY `id_image` (`id_image`) /*!80000 INVISIBLE */,
+  KEY `id_fact_idx` (`id_espece`),
+  KEY `id_etat_idx` (`id_etat`),
+  CONSTRAINT `id_espece` FOREIGN KEY (`id_espece`) REFERENCES `wildlens_facts` (`id_espece`),
+  CONSTRAINT `id_etat` FOREIGN KEY (`id_etat`) REFERENCES `wildlens_etat` (`id_etat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- AUTO_INCREMENT for table `wildlens_d2_zone`
+-- Dumping data for table `wildlens_images`
 --
-ALTER TABLE `wildlens_d2_zone`
-  MODIFY `id_zone` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `wildlens_d3_especes`
---
-ALTER TABLE `wildlens_d3_especes`
-  MODIFY `id_espece` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+LOCK TABLES `wildlens_images` WRITE;
+/*!40000 ALTER TABLE `wildlens_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wildlens_images` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- AUTO_INCREMENT for table `wildlens_facts`
---
-ALTER TABLE `wildlens_facts`
-  MODIFY `id_fact` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-01 13:08:46
